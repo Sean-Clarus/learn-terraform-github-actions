b853739f91c2356154045d7806a9ca25cb757ee5
Sean A <serclarus@gmail.com>
2023-07-10 12:35:41 -0400

    added dummy line

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
+# dummy line
+
 resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
